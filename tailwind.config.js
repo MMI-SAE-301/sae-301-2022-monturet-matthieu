185468e34ae320717bb6506a0092591e69a75aed
Matthieu Monturet <matthieu.monturet@edu.univ-fcomte.fr>
2022-10-25 15:52:10 +0200

    importation styles figma

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,31 @@ const formKitTailwind = require("@formkit/themes/tailwindcss");
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "Noir": "#1c1717",
+        "Bleu": "#151f43",
+        "Blanc": "#ebeff0",
+        "Jaune": "#8f6d10"
+       },
+       fontSize: {
+        "base": "1rem",
+        "lg": "1.125rem",
+        "xl": "1.625rem"
+       },
+       fontFamily: {
+        "piazzolla": "Piazzolla",
+        "poppins": "Poppins",
+        "montserrat": "Montserrat"
+       },
+       borderRadius: {
+        "none": "0",
+        "xs": "0.3125rem",
+        "sm": "0.9375rem",
+        "default": "1.46875rem",
+        "lg": "2.125rem"
+       },
+    },
   },
   plugins: [
     require("@tailwindcss/typography"),
