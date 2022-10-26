99cdd97089475e9f797dbf83806295ceacc0d33e
Matthieu Monturet <matthieu.monturet@edu.univ-fcomte.fr>
2022-10-26 08:36:22 +0200

    Composant header mobile&desktop

---
+++ src/components/Header.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import Logo from "@/components/icons/Logo.vue";
+import LightMode from "@/components/icons/LightMode.vue";
+import { Bars3Icon } from "@heroicons/vue/20/solid";
+
+const props = defineProps({
+  menuState: { type: Boolean, default: false },
+});
+</script>
+
+<template>
+  <div class="flex justify-between">
+    <div class="flex items-center h-12 space-x-3 ml-4">
+      <Logo class="h-10 w-24 fill-Noir" />
+      <LightMode class="h-10 w-6"/>
+    </div>
+    <!--Menu du header : MOBILE -->
+    <div class="flex items-center mr-4 md:hidden">
+      <button @click="menuState = !menuState">
+        <Bars3Icon class="h-10 w-8" />
+      </button>
+    </div>
+  </div>
+
+  <!--Menu du header : CONTENU DU MENU MOBILE -->
+  <section :class="{ hidden: menuState }" class="block w-full bg-Bleu text-Blanc md:hidden">
+    <div class="flex flex-col items-center justify-center gap-6 p-4 text-lg font-poppins">
+      <RouterLink to="/Montre/" class="w-fit p-2 border-2 rounded-md" @click="menuState = !menuState">
+        <p class="w-fit">La SmartWatch</p>
+      </RouterLink>
+      
+      <RouterLink to="/Montre/new" class="w-fit px-4 py-2 border-2 rounded-md" @click="menuState = !menuState">
+        <p class="w-fit">Personnaliser</p>
+      </RouterLink>
+      
+      <RouterLink to="/compteview" class="w-fit px-5 py-2 border-2 rounded-md" @click="menuState = !menuState">
+        <p class="w-fit">Mon compte</p>
+      </RouterLink>
+    </div>
+  </section>
+</template>
---
+++ src/pages/Montre/new.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import Header from '@/components/Header.vue'
+import Logo from '@/components/icons/Logo.vue'
+import LightMode from '@/components/icons/LightMode.vue'
+
+</script>
+
+<template>
+    <section class="bg-Blanc dark:bg-Noir">
+        <div class="md:hidden">
+            <Header />
+        </div>
+
+        <div class="hidden flex-row items-center justify-between gap-4 md:flex button">
+            <div class="flex space-x-4 ml-7">
+                <Logo class="h-10 w-24 fill-Noir dark:fill-Blanc" />
+                <LightMode class="h-10 w-6"/>
+            </div>
+            <div class="flex flex-row gap-12 mr-7">
+                <RouterLink to="/Montre/" class="w-fit">
+                    <p class="w-fit">La SmartWatch</p>
+                </RouterLink>
+                
+                <RouterLink to="/Montre/new" class="w-fit border-b-4 border-Bleu">
+                    <p class="w-fit">Personnaliser</p>
+                </RouterLink>
+                
+                <RouterLink to="/compteview" class="w-fit">
+                    <p class="w-fit">Mon compte</p>
+                </RouterLink>
+            </div>
+        </div>
+    </section>
+</template>
