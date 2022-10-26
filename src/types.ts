d22342ba15230876305a19099d567843fa5be3f1
Matthieu Monturet <matthieu.monturet@edu.univ-fcomte.fr>
2022-10-26 16:58:25 +0200

    création composants svg montres personnalisation + types

---
+++ src/types.ts
@@ -0,0 +1,63 @@
+export interface Montre {
+    ecran?: string;
+    boitier?: string;
+    bracelet?: string;
+}
+
+export const colors = {
+    "#1C1717": "Noir",
+    "#202431": "Gris foncé",
+    "#471458": "Violet foncé",
+    "#710783": "Violet",
+    "#132E90": "Bleu foncé",
+    "#364EA7": "Bleu roi",
+    "#3B90CE": "Bleu",
+    "#74D7ED": "Bleu clair",
+    "#09F24A": "Vert clair",
+    "#099006": "Vert",
+    "#035101": "Vert foncé",
+    "#7A4900": "Marron",
+    "#BB8900": "Marron-jaune",
+    "#FFF500": "Jaune fluo",
+    "#FCB500": "Jaune",
+    "#DA8300": "Jaune sombre",
+    "#DD6A00": "Jaune orangé",
+    "#FF8A00": "Orange pâle",
+    "#FF5C00": "Orange",
+    "#EA5400": "Orange foncé",
+    "#A10A00": "Rouge bordeaux",
+    "#D30000": "Rouge foncé",
+    "#FF0000": "Rouge",
+    "#FF5C5C": "Rouge pâle",
+    "#C9BEBE": "Gris",
+    "#EBEFF0": "Blanc"   
+}
+
+export const nuances = {
+    "#000000": "Noir total",
+    "#1C1717": "Noir",
+    "#272020": "Gris très sombre",
+    "#303135": "Gris sombre",
+    "#424242": "Gris pastel",
+    "#525252": "Gris",
+    "#636363": "Gris",
+    "#727272": "Gris clair",
+    "#797979": "Gris clair",
+    "#7E7E7E": "Gris très clair",
+    "#888888": "Gris très clair",
+    "#8E8E8E": "Gris très clair",
+    "#999999": "Gris pâle",
+    "#A4A2A2": "Gris pâle",
+    "#A7A7A7": "Gris très pâle",
+    "#ACACAC": "Gris très pâle",
+    "#B1B1B1": "Blanc sombre",
+    "#B8B8B8": "Blanc sombre",
+    "#BCBCBC": "Blanc cassé",
+    "#C0C0C0": "Blanc cassé",
+    "#C6C6C6": "Blanc éludé",
+    "#CFCFCF": "Blanc éludé",
+    "#D9D9D9": "Blanc-gris",
+    "#E6E6E6": "Blanc-gris clair",
+    "#F2F2F2": "Blanc-gris très clair",
+    "#EBEFF0": "Blanc"
+}
